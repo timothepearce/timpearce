e18776f41076b15a95fe88031975ce57cf5b3f6a
Timothé Pearce <timothe.pearce@gmail.com>
2021-08-11 23:35:02 +0200

    Fix last link

--- components/Header.vue
+++ components/Header.vue
@@ -43,7 +43,7 @@
                 <NuxtLink to="/about" @click.native="toggleMenu()" class="block font-bold no-underline text-gray-800">About</NuxtLink>
                 <NuxtLink to="/articles" @click.native="toggleMenu()" class="block font-bold no-underline text-gray-800">Articles</NuxtLink>
                 <NuxtLink to="/projects" @click.native="toggleMenu()" class="block font-bold no-underline text-gray-800">Projects</NuxtLink>
-                <a href="https://www.malt.fr/profile/timothepearce" target="_blank" class="flex items-center font-bold no-underline text-gray-800">
+                <a @click="toggleMenu()" href="https://www.malt.fr/profile/timothepearce" target="_blank" class="flex items-center font-bold no-underline text-gray-800">
                     Hire me
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
