464e0709cea57dbfccd805a9b764854094e9fe75
Timothé Pearce <timothe.pearce@gmail.com>
2021-08-11 23:20:15 +0200

    Extract Footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,15 +11,7 @@
                 </div>
             </div>
 
-            <div class="flex flex-wrap justify-end text-xs space-x-2 text-gray-400">
-                <a target="_blank" href="mailto:timothe.pearce@gmail.com" class="font-semibold no-underline hover:text-gray-800">Email</a>
-                <span>-</span>
-                <a target="_blank" href="https://github.com/TimothePearce" class="font-semibold no-underline hover:text-gray-800">GitHub</a>
-                <span>-</span>
-                <a target="_blank" href="https://www.linkedin.com/in/timothepearce" class="font-semibold no-underline hover:text-gray-800">LinkedIn</a>
-                <span>-</span>
-                <a target="_blank" href="https://twitter.com/TimothePearce" class="font-semibold no-underline hover:text-gray-800">Twitter</a>
-            </div>
+            <Footer />
         </div>
     </div>
 </template>
