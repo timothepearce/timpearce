bb76503754fc3cbbcdcbd0c10529e49bc2db4bd3
Timothé Pearce <timothe.pearce@gmail.com>
2021-08-12 15:14:47 +0200

    Implements svg

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="max-w-7xl mx-auto py-3">
-        <div class="max-w-none mx-auto">
-            <div class="bg-white overflow-hidden rounded-lg shadow">
-                <div class="bg-white px-4 py-5 sm:px-6">
-                    <div class="flex space-x-3">
-                        <div class="flex-shrink-0">
-                            <img class="h-10 w-10 rounded-md" :src="logoUrl" alt="logo">
-                        </div>
+    <div class="bg-white overflow-hidden rounded-lg shadow">
+        <div class="bg-white px-4 py-5 sm:px-6">
+            <div class="flex space-x-3">
+                <div class="flex-shrink-0">
+                    <img class="h-10 w-10 rounded-md" :src="logoUrl" alt="logo">
+                </div>
 
-                        <div class="min-w-0 flex-1">
-                            <p class="text-sm font-medium text-gray-800">
-                                <a href="#" class="hover:underline">{{ name }}</a>
-                            </p>
+                <div class="min-w-0 flex-1">
+                    <p class="text-sm font-medium text-gray-800">
+                        <a href="#" class="hover:underline">{{ name }}</a>
+                    </p>
 
-                            <p class="text-sm text-gray-500">{{ dates }}</p>
-                        </div>
+                    <p class="text-sm text-gray-500">{{ dates }}</p>
+                </div>
 
-                        <div class="flex-shrink-0 self-center flex">
-                            <div class="relative z-30 inline-block text-left">
-                                <button type="button" class="-m-2 p-2 rounded-full flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
+                <div class="flex-shrink-0 self-center flex">
+                    <div class="relative z-30 inline-block text-left">
+                        <button @click="toggle()" type="button" class="-m-2 p-2 rounded-full flex items-center">
+                            <svg v-if="!open" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -48,6 +48,18 @@ export default {
         dates: {
             type: String,
             required: true,
+        }
+    },
+
+    data() {
+        return {
+            open: false,
+        }
+    },
+
+    methods: {
+        toggle() {
+            this.open = !this.open;
         }
     }
 }
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,27 +1,34 @@
 <template>
     <div>
         <h1 class="text-2xl font-extrabold">Projects</h1>
-        <div class="text-lg text-gray-700 font-light space-y-6 mt-6">
+        <div class="text-lg text-gray-700 font-light mt-6">
             <p>Here you’ll find a list of all the important projects I worked on through the last years.</p>
         </div>
 
-        <ProjectCard
-            name="Les petits bots"
-            dates="June 2018 to now"
-            logoUrl="https://avatars.githubusercontent.com/u/23485669?s=200&v=4"
-            class="mt-6"
-        ></ProjectCard>
+        <div class="mt-6 space-y-6">
+            <ProjectCard
+                name="Les petits bots"
+                dates="From June 2018 until today"
+                logoUrl="https://avatars.githubusercontent.com/u/39500458?s=200&v=4"
+            ></ProjectCard>
 
-        <ProjectCard
-            name="Evaneos"
-            dates="June 2018 to now"
-            logoUrl="https://avatars.githubusercontent.com/u/23485669?s=200&v=4"
-        ></ProjectCard>
+            <ProjectCard
+                name="HUSH"
+                dates="From October 2019 to January 2020"
+                logoUrl="https://raw.githubusercontent.com/TimothePearce/hush/5452bd646af59cbe225603bd4942c8b0b793dcf5/public/images/icones/h.svg?token=ACZG4XILL6CIYKNB7MWCXHTBCUPBS"
+            ></ProjectCard>
 
-        <ProjectCard
-            name="Doplr"
-            dates="June 2018 to now"
-            logoUrl="https://avatars.githubusercontent.com/u/23485669?s=200&v=4"
-        ></ProjectCard>
+            <ProjectCard
+                name="Evaneos"
+                dates="From June 2018 until today"
+                logoUrl="https://avatars.githubusercontent.com/u/344797?s=200&v=4"
+            ></ProjectCard>
+
+            <ProjectCard
+                name="Doplr"
+                dates="From June 2018 until today"
+                logoUrl="https://avatars.githubusercontent.com/u/23485669?s=200&v=4"
+            ></ProjectCard>
+        </div>
     </div>
 </template>
