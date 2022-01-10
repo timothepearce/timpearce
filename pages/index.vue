3c1e6da1ba7533e950d81617cbe9191b19e3542e
TimothePearce <timothe.pearce@gmail.com>
2022-01-10 15:17:04 +0100

    Update link

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
     <div>
         <h1 class="text-2xl font-extrabold">Who am I?</h1>
         <div class="text-lg text-gray-700 font-light space-y-8 mt-6">
-            <p>Hi, I’m <a target="_blank" href="https://twitter.com/TimothePearce" class="font-semibold underline text-gray-800">@TimothePearce</a>, a software engineer (full-stack developer) based in Paris.</p>
+            <p>Hi, I’m <a target="_blank" href="https://twitter.com/TimothePearce" class="font-semibold underline text-gray-800">@timothepearce</a>, a software engineer (full-stack developer) based in Paris.</p>
 
             <p>I spent the last seven years studying computer science, gaining a wide variety of skills, from front-end development to DevOps, and building reliable software solutions.</p>
 
             <p>What do I do now? I am CTO for the French company <a target="_blank" href="https://lespetitsbots.com" class="text-gray-800 font-semibold underline">Les petits bots</a>, where I build chatbots and explore the wonderful <span class="line-through">but unfinished</span> world of Machine Learning, especially Natural Language Processing.</p>
 
-            <p>If you are interested in learning more about me, you can check my <NuxtLink to="/projects" class="font-semibold underline text-gray-800">projects</NuxtLink>, visit my <a target="_blank" href="https://github.com/TimothePearce" class="font-semibold underline text-gray-800">GitHub profile</a> or <a target="_blank" href="mailto:timothe.pearce@gmail.com" class="text-gray-800 font-semibold underline">email me</a>. If you’d like to work with me, I’m also available as a <a target="_blank" href="https://www.malt.fr/dashboard/freelancer" class="text-gray-800 font-semibold underline">freelancer</a>.</p>
+            <p>If you are interested in learning more about me, you can check my <NuxtLink to="/projects" class="font-semibold underline text-gray-800">projects</NuxtLink>, visit my <a target="_blank" href="https://github.com/timothepearce" class="font-semibold underline text-gray-800">GitHub profile</a> or <a target="_blank" href="mailto:timothe.pearce@gmail.com" class="text-gray-800 font-semibold underline">email me</a>. If you’d like to work with me, I’m also available as a <a target="_blank" href="https://www.malt.fr/dashboard/freelancer" class="text-gray-800 font-semibold underline">freelancer</a>.</p>
         </div>
     </div>
 </template>
